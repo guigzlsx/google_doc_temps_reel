f6632e19184d8354a3a1d48dfb5a38c6d8b31e16
Guillaume Lsx <guillaumelsx@MacBook-Pro-de-Guillaume.local>
2025-07-09 15:56:54 +0200

    remove images

--- server.js
+++ server.js
@@ -1,54 +1,26 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const path = require("path");
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 // Serve static files (frontend)
-app.use(express.static(path.join(__dirname, "public")));
-// Sert les fichiers .mjs avec le bon type MIME pour les imports ES modules
-app.use("/node_modules", (req, res, next) => {
-  if (req.path.endsWith(".mjs")) {
-    res.type("application/javascript");
-  }
-  next();
-});
-app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-let currentText = "";
+let currentText = '';
 
-io.on("connection", (socket) => {
-  console.log("[serveur] Nouveau client connecté, id :", socket.id);
+io.on('connection', (socket) => {
   // Envoie le texte actuel au nouvel arrivant
-  socket.on("get_text", () => {
-    console.log(
-      "[serveur] get_text reçu de",
-      socket.id,
-      "| currentText =",
-      currentText
-    );
-    socket.emit("init_text", currentText);
+  socket.on('get_text', () => {
+    socket.emit('init_text', currentText);
   });
 
-  // Nouvelle gestion : opérations atomiques
-  socket.on("operation", (op) => {
-    // Appliquer l'opération à currentText
-    if (op.type === "insert") {
-      currentText =
-        currentText.slice(0, op.pos) + op.value + currentText.slice(op.pos);
-    } else if (op.type === "delete") {
-      currentText =
-        currentText.slice(0, op.pos) + currentText.slice(op.pos + op.length);
-    }
-    // Diffuser l'opération aux autres clients
-    socket.broadcast.emit("operation", op);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("[serveur] Client déconnecté, id :", socket.id);
+  socket.on('text_update', (html) => {
+    currentText = html;
+    socket.broadcast.emit('text_update', html);
   });
 });
 
